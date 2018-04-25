a3713dac06ac22aa4406f3416ac67e80d179ad52
srijanreddy98 <srijanreddy98@gmail.com>
2018-04-25 22:24:49 +0530

    New

--- keys/keys.js
+++ keys/keys.js
@@ -1,8 +1,8 @@
 var keys = {
   GoogleclientID: "230755439544-1d8l9g5p1d9nq0ga2uiv8no5kjp4p13c.apps.googleusercontent.com",
   Googleclient_secret: "mWe_Ca5ZKClW8QbHAhh7lSOG",
-  FacebookClientID: "386462238429564",
-  FacebookClient_secret: "d0a67e92dcd3e396fb8b3a2848881841",
+  FacebookClientID: "159328991412623",
+  FacebookClient_secret: "f64e9a4bebda5ee7c283b79095e0aea5",
   cookieKey: 'iimindoreerpmadebysrijanreddy'
 }
 module.exports = {
