6888dd9fbfb0d9ea7df8ad9cd8b559c896647b10
srijanreddy98 <srijanreddy98@gmail.com>
2018-04-25 21:47:09 +0530

    New

---
+++ keys/keys.js
@@ -0,0 +1,10 @@
+var keys = {
+  GoogleclientID: "230755439544-1d8l9g5p1d9nq0ga2uiv8no5kjp4p13c.apps.googleusercontent.com",
+  Googleclient_secret: "mWe_Ca5ZKClW8QbHAhh7lSOG",
+  FacebookClientID: "386462238429564",
+  FacebookClient_secret: "d0a67e92dcd3e396fb8b3a2848881841",
+  cookieKey: 'iimindoreerpmadebysrijanreddy'
+}
+module.exports = {
+  keys
+}
